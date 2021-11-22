22d7a987a921ea1faab2a4686d9731039045c0bd
Laxmi Sharma <laxminla@yahoo.com>
2021-11-21 20:26:09 -0800

    updated schemas

--- server/server.js
+++ server/server.js
@@ -5,8 +5,7 @@ const db = require('./config/connection');
 const { typeDefs, resolvers } = require('./schemas');
 const { authMiddleware } = require('./utils/auth');
 const app = express();
-const routes = require('./routes');
-
+// const routes = require('./routes');
 const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
   typeDefs,
